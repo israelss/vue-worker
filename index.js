a94bf85c34295f708e31fe3672ed173396531e14
Israel <israel.santanna@gmail.com>
2017-05-22 00:54:34 -0300

    Upgrade dep version & tweak

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import SimpleWebWorker from 'simple-web-worker'
 
 export default {
   install: function(Vue, name) {
-    if (! name) name = '$worker';
-
+    name = name || '$worker'
     Object.defineProperty(Vue.prototype, name, { value: SimpleWebWorker })
   }
 }
