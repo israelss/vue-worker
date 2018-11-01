2955231b7ef768c25c1c40ebf409664d9dcd585b
Bernix01 <gbernal096@gmail.com>
2018-11-01 13:25:44 -0500

    1

---
+++ types/index.d.ts
@@ -0,0 +1,4 @@
+import _Vue, { WatchOptions } from "vue";
+import './vue';
+
+export declare function install(Vue: typeof _Vue): void;
---
+++ types/vue.d.ts
@@ -0,0 +1,6 @@
+declare module "vue/types/vue" {
+    interface Vue {
+      $worker: any;
+    }
+  }
+
